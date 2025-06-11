178f4fa2ae7859dddda450e32ea26b9ad4a04572
Allen Zheng <zhenga8533@gmail.com>
2025-06-11 09:45:39 -0400

    Migrated economy configs

--- src/main/java/net/volcaronitee/volcclient/config/category/EconomyConfig.java
+++ src/main/java/net/volcaronitee/volcclient/config/category/EconomyConfig.java
@@ -1,9 +1,12 @@
 package net.volcaronitee.volcclient.config.category;
 
 import dev.isxander.yacl3.api.ConfigCategory;
+import dev.isxander.yacl3.api.NameableEnum;
 import dev.isxander.yacl3.api.Option;
 import dev.isxander.yacl3.api.OptionDescription;
 import dev.isxander.yacl3.api.OptionGroup;
+import dev.isxander.yacl3.api.controller.BooleanControllerBuilder;
+import dev.isxander.yacl3.api.controller.IntegerSliderControllerBuilder;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import net.minecraft.text.Text;
 import net.volcaronitee.volcclient.config.VolcClientConfig;
@@ -12,22 +15,125 @@ public class EconomyConfig {
     public static ConfigCategory create(VolcClientConfig defaults, VolcClientConfig config) {
         return ConfigCategory.createBuilder().name(Text.literal("Economy"))
 
-                // Option Group
-                .group(OptionGroup.createBuilder().name(Text.literal(""))
+                // Economy Option Group
+                .group(OptionGroup.createBuilder().name(Text.literal("Economy"))
 
-                        // Option
-                        .option(Option.<Boolean>createBuilder().name(Text.literal(""))
-                                .description(OptionDescription.of(Text.literal("")))
-                                .binding(defaults.economy.temp, () -> config.economy.temp,
-                                        newVal -> config.economy.temp = newVal)
+                        // Coin Tracker
+                        .option(Option.<Integer>createBuilder().name(Text.literal("Coin Tracker"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Tracks coin purse gain/loss progress on the screen. Sets time in seconds of inactivity before tracking stops.")))
+                                .binding(defaults.economy.coinTracker,
+                                        () -> config.economy.coinTracker,
+                                        newVal -> config.economy.coinTracker = newVal)
+                                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                        .range(0, 10).step(1))
+                                .build())
+
+                        // No Bits Warning
+                        .option(Option.<Boolean>createBuilder()
+                                .name(Text.literal("No Bits Warning"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Displays a chat warning and title when bits pool is empty.")))
+                                .binding(defaults.economy.noBitsWarning,
+                                        () -> config.economy.noBitsWarning,
+                                        newVal -> config.economy.noBitsWarning = newVal)
                                 .controller(VolcClientConfig::createBooleanController).build())
+
+                        .build())
+
+                // Item Value Option Group
+                .group(OptionGroup.createBuilder().name(Text.literal("Item Value"))
+
+                        // Container Value
+                        .option(Option.<Integer>createBuilder()
+                                .name(Text.literal("Container Value"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Displays item values in container inventories. Set number of items to display before cutoff.")))
+                                .binding(defaults.economy.containerValue,
+                                        () -> config.economy.containerValue,
+                                        newVal -> config.economy.containerValue = newVal)
+                                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                        .range(0, 54).step(1))
+                                .build())
+
+                        // Item Price
+                        .option(Option.<EconomyConfig.ItemPrice>createBuilder()
+                                .name(Text.literal("Item Price"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Sets how item prices are displayed in the game.")))
+                                .binding(defaults.economy.itemPrice, () -> config.economy.itemPrice,
+                                        newVal -> config.economy.itemPrice = newVal)
+                                .controller(VolcClientConfig::createEnumController).build())
+
+                        // Single Attribute
+                        .option(Option.<Boolean>createBuilder()
+                                .name(Text.literal("Single Attribute"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Displays only a single attribute for item prices.")))
+                                .binding(defaults.economy.singleAttribute,
+                                        () -> config.economy.singleAttribute,
+                                        newVal -> config.economy.singleAttribute = newVal)
+                                .controller(VolcClientConfig::createBooleanController).build())
+
+                        // Price Type
+                        .option(Option.<Boolean>createBuilder().name(Text.literal("Price Type"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Sets the type of bazaar pricing used in item calculations.")))
+                                .binding(defaults.economy.priceType, () -> config.economy.priceType,
+                                        newVal -> config.economy.priceType = newVal)
+                                .controller(opt -> VolcClientConfig.createBooleanController(opt,
+                                        "Order", "Insta"))
+                                .build())
+
+                        // Trade Evaluation
+                        .option(Option.<Boolean>createBuilder()
+                                .name(Text.literal("Trade Evaluation"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Enables value comparison display when in trade menus.")))
+                                .binding(defaults.economy.tradeEvaluation,
+                                        () -> config.economy.tradeEvaluation,
+                                        newVal -> config.economy.tradeEvaluation = newVal)
+                                .controller(opt -> BooleanControllerBuilder.create(opt)).build())
 
                         .build())
 
                 .build();
     }
 
-    // Option Group
+    // Economy Option Group
     @SerialEntry
-    public boolean temp = false;
+    public int coinTracker = 0;
+
+    @SerialEntry
+    public boolean noBitsWarning = true;
+
+    // Item Value Option Group
+    @SerialEntry
+    public int containerValue = 0;
+
+    @SerialEntry
+    public ItemPrice itemPrice = ItemPrice.OFF;
+
+    public enum ItemPrice implements NameableEnum {
+        OFF, ADVANCED, TOOLTIP, OMNI;
+
+        @Override
+        public Text getDisplayName() {
+            return switch (this) {
+                case OFF -> Text.literal("Disabled");
+                case ADVANCED -> Text.literal("Advanced View");
+                case TOOLTIP -> Text.literal("Tooltip View");
+                case OMNI -> Text.literal("Omnipotent View");
+            };
+        }
+    }
+
+    @SerialEntry
+    public boolean singleAttribute = true;
+
+    @SerialEntry
+    public boolean priceType = true; // true = Order, false = Insta
+
+    @SerialEntry
+    public boolean tradeEvaluation = false;
 }
