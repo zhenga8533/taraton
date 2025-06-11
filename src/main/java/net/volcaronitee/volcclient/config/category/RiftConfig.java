429f5b67298332636692da6afdefdf9e58ee5c5c
Allen Zheng <zhenga8533@gmail.com>
2025-06-11 16:49:57 -0400

    Migrated rift config

--- src/main/java/net/volcaronitee/volcclient/config/category/RiftConfig.java
+++ src/main/java/net/volcaronitee/volcclient/config/category/RiftConfig.java
@@ -4,22 +4,84 @@ import dev.isxander.yacl3.api.ConfigCategory;
 import dev.isxander.yacl3.api.Option;
 import dev.isxander.yacl3.api.OptionDescription;
 import dev.isxander.yacl3.api.OptionGroup;
+import dev.isxander.yacl3.api.controller.IntegerSliderControllerBuilder;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import net.minecraft.text.Text;
 import net.volcaronitee.volcclient.config.VolcClientConfig;
+import net.volcaronitee.volcclient.config.VolcClientConfig.AnnounceWP;
 
 public class RiftConfig {
     public static ConfigCategory create(VolcClientConfig defaults, VolcClientConfig config) {
         return ConfigCategory.createBuilder().name(Text.literal("Rift"))
 
-                // Option Group
-                .group(OptionGroup.createBuilder().name(Text.literal(""))
+                // Rift Option Group
+                .group(OptionGroup.createBuilder().name(Text.literal("Rift Tasks"))
 
-                        // Option
-                        .option(Option.<Boolean>createBuilder().name(Text.literal(""))
-                                .description(OptionDescription.of(Text.literal("")))
-                                .binding(defaults.rift.temp, () -> config.rift.temp,
-                                        newVal -> config.rift.temp = newVal)
+                        // DDR Helper
+                        .option(Option.<Boolean>createBuilder().name(Text.literal("DDR Helper"))
+                                .description(OptionDescription.of(Text
+                                        .literal("Replaces Dance Room titles with custom ones.")))
+                                .binding(defaults.rift.ddrHelper, () -> config.rift.ddrHelper,
+                                        newVal -> config.rift.ddrHelper = newVal)
+                                .controller(VolcClientConfig::createBooleanController).build())
+
+                        // Enigma Soul Waypoints
+                        .option(Option.<Integer>createBuilder()
+                                .name(Text.literal("Enigma Soul Waypoints"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Renders waypoints of Enigma Soul locations. Sets maximum distance for waypoints to be rendered.")))
+                                .binding(defaults.rift.enigmaSoulWaypoints,
+                                        () -> config.rift.enigmaSoulWaypoints,
+                                        newVal -> config.rift.enigmaSoulWaypoints = newVal)
+                                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                        .range(0, 128).step(4))
+                                .build())
+
+                        // Montezuma Soul Waypoints
+                        .option(Option.<Integer>createBuilder()
+                                .name(Text.literal("Montezuma Soul Waypoints"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Renders waypoints for nearby discord kittens. Sets maximum distance for waypoints to be rendered.")))
+                                .binding(defaults.rift.montezumaSoulWaypoints,
+                                        () -> config.rift.montezumaSoulWaypoints,
+                                        newVal -> config.rift.montezumaSoulWaypoints = newVal)
+                                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                        .range(0, 128).step(4))
+                                .build())
+
+                        .build())
+
+                // Vampire Slayer Option Group
+                .group(OptionGroup.createBuilder().name(Text.literal("Vampire Slayer"))
+
+                        // Announce Mania Phase
+                        .option(Option.<AnnounceWP>createBuilder()
+                                .name(Text.literal("Announce Mania Phase"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Sends a chat message on start of a mania phase.")))
+                                .binding(defaults.rift.announceManiaPhase,
+                                        () -> config.rift.announceManiaPhase,
+                                        newVal -> config.rift.announceManiaPhase = newVal)
+                                .controller(VolcClientConfig::createEnumController).build())
+
+                        // Effigy Waypoint
+                        .option(Option.<Boolean>createBuilder()
+                                .name(Text.literal("Effigy Waypoint"))
+                                .description(OptionDescription.of(Text
+                                        .literal("Renders a waypoint on inactive Blood Effigies.")))
+                                .binding(defaults.rift.effigyWaypoint,
+                                        () -> config.rift.effigyWaypoint,
+                                        newVal -> config.rift.effigyWaypoint = newVal)
+                                .controller(VolcClientConfig::createBooleanController).build())
+
+                        // Vampire Attack Display
+                        .option(Option.<Boolean>createBuilder()
+                                .name(Text.literal("Vampire Attack Display"))
+                                .description(OptionDescription.of(Text.literal(
+                                        "Displays time of Mania, Twinclaws, and Ichor attacks.")))
+                                .binding(defaults.rift.vampireAttackDisplay,
+                                        () -> config.rift.vampireAttackDisplay,
+                                        newVal -> config.rift.vampireAttackDisplay = newVal)
                                 .controller(VolcClientConfig::createBooleanController).build())
 
                         .build())
@@ -27,7 +89,23 @@ public class RiftConfig {
                 .build();
     }
 
-    // Option Group
+    // Rift Tasks Option Group
     @SerialEntry
-    public boolean temp = false;
+    public boolean ddrHelper = false;
+
+    @SerialEntry
+    public int enigmaSoulWaypoints = 0;
+
+    @SerialEntry
+    public int montezumaSoulWaypoints = 0;
+
+    // Vampire Slayer Option Group
+    @SerialEntry
+    public AnnounceWP announceManiaPhase = AnnounceWP.OFF;
+
+    @SerialEntry
+    public boolean effigyWaypoint = false;
+
+    @SerialEntry
+    public boolean vampireAttackDisplay = false;
 }
