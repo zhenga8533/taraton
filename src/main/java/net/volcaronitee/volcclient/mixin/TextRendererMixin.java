83f53d8ddf1cc6b6982bfafc8ebe25bec9dbc2e1
Allen Zheng <zhenga8533@gmail.com>
2025-06-17 00:54:27 -0400

    Initial text replacement test

--- src/main/java/net/volcaronitee/volcclient/mixin/TextRendererMixin.java
+++ src/main/java/net/volcaronitee/volcclient/mixin/TextRendererMixin.java
@@ -1,28 +1,22 @@
 package net.volcaronitee.volcclient.mixin;
 
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.text.OrderedText;
 import net.minecraft.text.Text;
 
 @Mixin(TextRenderer.class)
 public class TextRendererMixin {
-    @Inject(method = "draw", at = @At("HEAD"), cancellable = true)
-    private void onDrawText(Text text, float x, float y, int color, boolean shadow,
-            Matrix4f matrices, VertexConsumerProvider vertexConsumers, int light,
-            CallbackInfoReturnable<Integer> cir) {
-        String replaced = text.getString().replaceAll("Volcaronitee", "The Lion");
-
-        if (!replaced.equals(text.getString())) {
-            Text newText = Text.literal(replaced);
-            int result = ((TextRenderer) (Object) this).draw(newText, x, y, color, shadow, matrices,
-                    vertexConsumers, TextRenderer.TextLayerType.NORMAL, light, 0);
-            cir.setReturnValue(result);
-        }
+    @Redirect(
+            method = "draw(Lnet/minecraft/text/Text;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;IIZ)I",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/text/Text;asOrderedText()Lnet/minecraft/text/OrderedText;"))
+    private OrderedText redirectAsOrderedText(Text originalText) {
+        String originalString = originalText.getString();
+        String replacedString = originalString.replaceAll("Volcaronitee", "The Lion");
+        Text newText = Text.literal(replacedString);
+        return newText.asOrderedText();
     }
 }
-
