d5a8069c0d0315ca2e1e94962ba5ffefd4e3d5db
Allen Zheng <zhenga8533@gmail.com>
2025-06-19 10:00:25 -0400

    Register PlaytimeWarning feature in FeatureUtil
    Added PlaytimeWarning registration to the FeatureUtil.init() method under chat features. This ensures the PlaytimeWarning feature is initialized alongside other features.

--- src/main/java/net/volcaronitee/volcclient/util/FeatureUtil.java
+++ src/main/java/net/volcaronitee/volcclient/util/FeatureUtil.java
@@ -1,10 +1,14 @@
 package net.volcaronitee.volcclient.util;
 
+import net.volcaronitee.volcclient.feature.chat.PlaytimeWarning;
 import net.volcaronitee.volcclient.feature.general.RemoveSelfieMode;
 
 public class FeatureUtil {
     public static void init() {
         // General Features
         RemoveSelfieMode.register();
+
+        // Chat Features
+        PlaytimeWarning.register();
     }
 }
