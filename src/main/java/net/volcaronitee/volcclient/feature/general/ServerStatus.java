5116da5306b907e51ff8074c2ad14915ca2943a4
Allen Zheng <zhenga8533@gmail.com>
2025-06-25 11:13:19 -0400

    Encapsulate singletons and add getter methods
    Changed INSTANCE fields in ServerStatus and SkyBlockStats to private and provided public static getInstance() methods for controlled access. Also added a getPing() method and internal setPing() logic to ServerStatus for improved encapsulation.

--- src/main/java/net/volcaronitee/volcclient/feature/general/ServerStatus.java
+++ src/main/java/net/volcaronitee/volcclient/feature/general/ServerStatus.java
@@ -1,7 +1,11 @@
 package net.volcaronitee.volcclient.feature.general;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.PlayerListEntry;
+
 public class ServerStatus {
-    public final static ServerStatus INSTANCE = new ServerStatus();
+    private final static ServerStatus INSTANCE = new ServerStatus();
 
     private int ping = 0;
     private int fps = 0;
@@ -11,4 +15,23 @@ public class ServerStatus {
     private float pitch = 0.0f;
     private int angle = 0;
     private float day = 0.0f;
+
+    public static ServerStatus getInstance() {
+        return INSTANCE;
+    }
+
+    public int getPing() {
+        return ping;
+    }
+
+    private void setPing() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
+        PlayerListEntry clientPlayer = networkHandler.getPlayerListEntry(client.player.getUuid());
+        if (clientPlayer != null) {
+            this.ping = clientPlayer.getLatency();
+        } else {
+            this.ping = 0;
+        }
+    }
 }
--- src/main/java/net/volcaronitee/volcclient/feature/general/SkyBlockStats.java
+++ src/main/java/net/volcaronitee/volcclient/feature/general/SkyBlockStats.java
@@ -1,9 +1,13 @@
 package net.volcaronitee.volcclient.feature.general;
 
 public class SkyBlockStats {
-    public static final SkyBlockStats INSTANCE = new SkyBlockStats();
+    private static final SkyBlockStats INSTANCE = new SkyBlockStats();
 
     private String pet = "None";
     private int soulflow = 0;
     private int legion = 0;
+
+    public static SkyBlockStats getInstance() {
+        return INSTANCE;
+    }
 }
