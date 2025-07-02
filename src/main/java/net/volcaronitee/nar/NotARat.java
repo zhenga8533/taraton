8b4d00d71250ba605e63558fb082c43a5f1f9625
Allen Zheng <zhenga8533@gmail.com>
2025-07-01 23:54:51 -0400

    Add ToggleUtil initialization to NotARat
    ToggleUtil.init() is now called during NotARat initialization to ensure ToggleUtil is properly set up with other utilities.

--- src/main/java/net/volcaronitee/nar/NotARat.java
+++ src/main/java/net/volcaronitee/nar/NotARat.java
@@ -14,6 +14,7 @@ import net.volcaronitee.nar.util.PlayerUtil;
 import net.volcaronitee.nar.util.ScheduleUtil;
 import net.volcaronitee.nar.util.TablistUtil;
 import net.volcaronitee.nar.util.TickUtil;
+import net.volcaronitee.nar.util.ToggleUtil;
 
 public class NotARat implements ClientModInitializer {
     public static final String MOD_ID = "nar";
@@ -36,6 +37,7 @@ public class NotARat implements ClientModInitializer {
         PlayerUtil.init();
         ScheduleUtil.init();
         TablistUtil.init();
+        ToggleUtil.init();
         TickUtil.init();
     }
 }
