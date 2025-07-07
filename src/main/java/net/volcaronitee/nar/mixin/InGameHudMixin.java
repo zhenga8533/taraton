86b8d17a87a8b00a9f49823b075a2440a57c7aad
Allen Zheng <zhenga8533@gmail.com>
2025-07-07 00:12:32 -0400

    Reformat Javadoc in InGameHudMixin
    Improves the formatting and alignment of the Javadoc comments in InGameHudMixin.java for better readability and consistency.

--- src/main/java/net/volcaronitee/nar/mixin/InGameHudMixin.java
+++ src/main/java/net/volcaronitee/nar/mixin/InGameHudMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
@@ -15,12 +16,12 @@ import net.volcaronitee.nar.config.NarConfig;
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
     /**
-     * Injects into the renderStatusEffectOverlay method to cancel the rendering of status effects
-     * from the HUD.
+     * Injects into the renderStatusEffectOverlay method to cancel the rendering of
+     * status effects from the HUD.
      * 
-     * @param context The DrawContext used for rendering.
+     * @param context     The DrawContext used for rendering.
      * @param tickCounter The RenderTickCounter for the current frame.
-     * @param ci The CallbackInfo for the injection.
+     * @param ci          The CallbackInfo for the injection.
      */
     @Inject(method = "renderStatusEffectOverlay", at = @At("HEAD"), cancellable = true)
     private void nar$hideStatusEffectsOnHud(DrawContext context, RenderTickCounter tickCounter,
