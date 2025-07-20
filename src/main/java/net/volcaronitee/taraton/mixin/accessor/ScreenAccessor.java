1a23fc0f42b99102d876561c75833d3219231cb5
Allen Zheng <zhenga8533@gmail.com>
2025-07-20 18:34:54 -0400

    Add ScreenAccessor mixin to expose addDrawableChild
    Introduces ScreenAccessor interface to provide access to the addDrawableChild method in the Screen class via mixin. Updates taraton.mixins.json to register the new accessor.

---
+++ src/main/java/net/volcaronitee/taraton/mixin/accessor/ScreenAccessor.java
@@ -0,0 +1,17 @@
+package net.volcaronitee.taraton.mixin.accessor;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
+import net.minecraft.client.gui.screen.Screen;
+
+/**
+ * Accessor mixin for the Screen class to expose the addDrawableChild method.
+ */
+@Mixin(Screen.class)
+public interface ScreenAccessor {
+    @Invoker("addDrawableChild")
+    <T extends Element & Drawable & Selectable> T invokeAddDrawableChild(T child);
+}
